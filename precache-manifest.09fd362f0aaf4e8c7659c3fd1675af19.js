44bfb8650d0562d069db18a3eba4f290ca0d125c
Barun Mishra <mishrabarun13@gmail.com>
2020-01-27 01:26:50 +0000

    Updates

---
+++ precache-manifest.09fd362f0aaf4e8c7659c3fd1675af19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485fd772a46696c52275f3093dfe21c8",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "843d221bfdd1f2d5ca96",
+    "url": "/TicTacToe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "9bcc8c8b373c4b556edc",
+    "url": "/TicTacToe/static/js/2.560fdbda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/TicTacToe/static/js/2.560fdbda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "843d221bfdd1f2d5ca96",
+    "url": "/TicTacToe/static/js/main.38a7d0e7.chunk.js"
+  },
+  {
+    "revision": "b8dedf07051127806fcf",
+    "url": "/TicTacToe/static/js/runtime-main.3f3f76df.js"
+  }
+]);
